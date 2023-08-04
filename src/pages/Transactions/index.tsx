e3001eabd75c0d4dc9a7a442360b30a25cb33c60
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-08-04 17:30:51 -0300

    feature: create Search Form

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,51 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import SearchForm from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactios() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>04/08/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Landing Page</td>
+              <td>
+                <PriceHighlight variant="income">R$ 3.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>01/08/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Conta AWS</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 8.000,00</PriceHighlight>
+              </td>
+              <td>Compra</td>
+              <td>02/08/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
