294e551a77e13250db80386d8d4a4fcfb9f1b947
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2023-08-04 16:48:51 -0300

    feature: create summary components

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 export function Transactios() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
